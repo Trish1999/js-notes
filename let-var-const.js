08c50ee88451389ac91fd9f6f4d9e21ba0bf9744
Trish1999 <trishnarani28@gmail.com>
2024-11-27 23:41:40 +0530

    two lessons

---
+++ let-var-const.js
@@ -0,0 +1,85 @@
+//var was introduced to js when it was created 
+//let and const introduced to js after ES6version come to market
+
+//Scope: 
+//certain area of a program where the defined variable is recognised beyond that it cant recognized
+//types of scope:
+//global scope,functionalscope, blog scope   etc
+
+//let and const are blog scope but var is global scope
+
+//let and const allows variable shadowing
+function test(){
+    let a="hello";
+
+    if(true){
+        let a="hi";
+        console.log(a);// here the hi will shadowing hello 
+    }
+    console.log(a);
+}
+test();
+
+//we can shawdow var variable by let but can not do the opposite 
+//if we try to do that its called illegal shadowing it will show variable is undefined
+
+function test(){
+    var a ="hello";
+    let b="bye";
+
+    if(true){
+        let a="hi";//var shodowing let (correct)
+        var b="goodbye";//let shadowing var (incorrect)
+        console.log(a);
+        console.log(b)// here the hi will shadowing hello 
+    }
+    console.log(a);
+}
+test();
+
+//we can redeclare a var as many time as we want 
+//but cant redeclare in the same scope
+
+var a;
+var a; //correct
+-------------------------
+{
+    let a;
+    let a;//incorrect
+}
+--------------------------
+let a;
+{
+    let a;//correct
+}
+
+//we can not declare a const without inisialization
+let a;
+var a;
+const a=5;//initialisation required
+
+//we can reinitialise let and var but can not reinitialise  const variable
+const a=5;
+a =10;//error
+
+//during the creation phase javascript  moves your variables and functions to top of code is called hoisting
+
+console.log(count);//undefined        | var count
+var count=1;                          | console.log(count);
+                                      | count=1;
+
+//hoisted in temporal deadzone:(time between the declaration and initialization of let and const variable)
+
+console.log(count);//error        
+let count=1;
+
+console.log(count);//error        
+const count=1;
+
+
+
+
+
+
+
+
---
+++ map-filter-reduce.js
@@ -0,0 +1,109 @@
+//map,filter and reduce are array method to iterate over an array and perform transformation and computation
+------------------------------------------MAP
+const nums=[1,2,3,4];
+
+const multiplay=nums.map((num,i,arr)=>{
+    return num*3+i;
+});
+
+console.log(multiplay);//[3,7,11,15]
+//map returns a new array
+---------------------------------------- FILTER
+const nums=[1,2,3,4];
+
+const moreThenTwo=nums.filter((num)=>{
+    return num >2;
+});
+
+console.log(moreThenTwo);//[3,4]
+//filters returns only those elements which fullfills the criteria
+-------------------------------------------REDUCE
+const nums=[1,2,3,4];
+
+const sum = nums.reduce((acc,curr,i,arr)=>{
+    return acc + curr
+},0);
+
+console.log(sum);//10
+
+//const sum = nums.reduce((accumulator,currentValue,index,arr)=>{},initialValue);
+//accumulator= result of the previous computation
+//currentValue= current element
+
+//reduce method reduces an array of values down to just one value
+=--------------------------------------
+//customMap function(polyfill)
+
+Array.prototype.myMap=function(cb){
+    let temp=[];
+    for(let i=0;i<this.length;i++){
+        temp.push(cb(this[i],i,this));
+    }
+  return temp;
+}
+
+//const multiplay=nums.myMap(()=>---)
+
+------------------------------------------
+//customFilter function(polyfill)
+
+Array.prototype.myFilter=function(cb){
+    let temp=[];
+    for(let i=0;i<this.length;i++){
+        if(cb(this[i],i,this)){
+        temp.push(this[i]);
+        }
+    }
+  return temp;
+}
+
+//const moreThenTwo=nums.myFilter(()=>----)
+----------------------------------------------------
+//customReduce function(polyfill)
+
+Array.prototype.myReduce=function(cb,initiaValue){
+    var accumulator = initiaValue;
+
+for(let i=0;i<this.length;i++){
+    accumulator=accumulator ? cb(accumulator,this[i], i,this): this[i];
+}
+return accumulator;
+};
+
+//const sum = nums.myReduce((acc,curr,i,arr)=>---)
+
+----------------------------------------------
+
+let students=[
+    {name:"minu"; rollNumber:10,marks:80},
+    {name:"sunu"; rollNumber:19,marks:90},
+    {name:"kunu"; rollNumber:31,marks:84},
+    {name:"tunu"; rollNumber:26,marks:30}
+]
+// return only name of the students in capital
+const a= students.map(stu=>stu.name.toUpperCase());
+
+// return only the details of those who scored more than 60 marks
+const b= students.filter((stu)=> stu.marks>60);
+
+//more than 60 marks and rollnumber greater then 15
+ const c=students((stu)=> stu.marks > 60 && stu.rollNumber>15);
+
+//sum of the all sudent marks
+const d=students.reducr((acc,curr)=>acc+curr.marks,0);
+
+//return only the names of students who scored more then 60
+const e=students.filter((stu)=>stu.marks>60).map(stu=>stu.name);
+
+//return total marks for students with marks greater than 60 
+//after2 marks have been added to those who scored less then 60
+
+const f=students.map((stu)=>{
+    if(stu.marks<60){
+        stu,marks+=20;
+    }
+
+    return stu;
+    })
+    .filter((stu)=>stu.marks>60)
+    .reduce((acc,curr)=>acc+curr.mark,0)
